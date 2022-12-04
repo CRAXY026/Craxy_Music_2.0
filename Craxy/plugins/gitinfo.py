e5170cb1b98168f5ec95a25c853d8913a21e43c4
KAkA026 <116966778+CRAXY026@users.noreply.github.com>
2022-12-05 00:49:42 +0530

    Update gitinfo.py

--- Craxy/plugins/gitinfo.py
+++ Craxy/plugins/gitinfo.py
@@ -6,7 +6,7 @@ from Craxy import app
 @app.on_message(filters.command(["github", "git"]))
 async def github(_, message):
     if len(message.command) != 2:
-        await message.reply_text("/git CraxymousR1025")
+        await message.reply_text("/git Craxy026")
         return
     username = message.text.split(None, 1)[1]
     URL = f'https://api.github.com/users/{username}'
