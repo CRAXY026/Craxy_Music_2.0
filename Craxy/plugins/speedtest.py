ad89ff46bd4505c612cc4b169d5f3f847ab5325e
KAkA026 <116966778+CRAXY026@users.noreply.github.com>
2022-12-04 23:51:50 +0530

    Update speedtest.py

--- Craxy/plugins/speedtest.py
+++ Craxy/plugins/speedtest.py
@@ -13,13 +13,13 @@ def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...**")
+        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ ᴅᴏsᴛ ᴡᴀɪᴛ 🥲...**")
         test.download()
-        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴜᴩʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ...**")
+        m = m.edit("**⇆ ʀᴜɴɴɪɴɢ ᴜᴩʟᴏᴀᴅ sᴩᴇᴇᴅᴛᴇsᴛ ᴅᴏsᴛ ᴡᴀɪᴛ 😅...**")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("**↻ sʜᴀʀɪɴɢ sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs...**")
+        m = m.edit("**↻ ᴅᴏsᴛ sʜᴀʀɪɴɢ sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs 😏...**")
     except Exception as e:
         return m.edit(e)
     return result
@@ -27,16 +27,16 @@ def testspeed(m):
 
 @app.on_message(filters.command(SPEEDTEST_COMMAND) & SUDOERS)
 async def speedtest_function(client, message):
-    m = await message.reply_text("💫 ᴛʀʏɪɴɢ ᴛᴏ ᴄʜᴇᴄᴋ ᴜᴩʟᴏᴀᴅ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅ...")
+    m = await message.reply_text("☛☚ ᴛʀʏɪɴɢ ᴛᴏ ᴄʜᴇᴄᴋ ᴜᴩʟᴏᴀᴅ ᴀɴᴅ ᴅᴏᴡɴʟᴏᴀᴅ sᴩᴇᴇᴅ...")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
     output = f"""✯ **sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛs** ✯
     
-<u>**❥͜͡ᴄʟɪᴇɴᴛ :**</u>
+<u>**⏤͟͟͞͞ᴄʟɪᴇɴᴛ :**</u>
 **» __ɪsᴩ :__** {result['client']['isp']}
 **» __ᴄᴏᴜɴᴛʀʏ :__** {result['client']['country']}
   
-<u>**❥͜͡sᴇʀᴠᴇʀ :**</u>
+<u>**⏤͟͟͞͞sᴇʀᴠᴇʀ :**</u>
 **» __ɴᴀᴍᴇ :__** {result['server']['name']}
 **» __ᴄᴏᴜɴᴛʀʏ :__** {result['server']['country']}, {result['server']['cc']}
 **» __sᴩᴏɴsᴏʀ :__** {result['server']['sponsor']}
