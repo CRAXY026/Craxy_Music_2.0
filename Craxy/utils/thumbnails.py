d4811b6fda932836a2f67dca65ae49f61da089ac
KAkA026 <116966778+CRAXY026@users.noreply.github.com>
2022-12-05 13:18:34 +0530

    Update thumbnails.py

--- Craxy/utils/thumbnails.py
+++ Craxy/utils/thumbnails.py
@@ -123,7 +123,7 @@ async def gen_thumb(videoid):
             font4 = ImageFont.truetype('Craxy/assets/font2.ttf', 35)
 
             image4 = ImageDraw.Draw(image2)
-            image4.text((10, 10), "ᴘᴏᴡᴇʀᴇᴅ ʙʏ : ⏤͟͟͞͞ Ｓ Δ Ｈ Ｉ Ｌ ♠", fill="white", font = font1, align ="left") 
+            image4.text((10, 10), "CRAXY BOI", fill="white", font = font1, align ="left") 
             image4.text((670, 150), "CRAXY PLAYER", fill="white", font = font2, stroke_width=2, stroke_fill="white", align ="left") 
 
             # title
